3642cce42d6e453dc9018aba57da4367769c7bba
Chandan Kumar <descentchandan@gmail.com>
2020-07-01 22:47:39 +0530

    starting js practice

--- other files/JSprctc.js
+++
@@ -1,8 +0,0 @@
-var input = document.getElementById("inp").value;
-console.log(input)
-var button = document.getElementById("but");
-var div = document.getElementById("d1");
-
-button.addEventListener("click", function () {
-  div.innerHTML = input;
-});
