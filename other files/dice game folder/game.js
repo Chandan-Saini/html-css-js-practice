8f9ec20aa68a17ab10493cbf2ade38f5a84f5367
Chandan Kumar <descentchandan@gmail.com>
2020-07-06 23:59:24 +0530

    trying using dice

--- other files/dice game folder/game.js
+++ other files/dice game folder/game.js
@@ -2,5 +2,8 @@
 var num= Math.floor(Math.random()*6) +1
 document.querySelector(".btn-roll").addEventListener("click", function () {
     dice = document.querySelector(".dice")
+    val = document.getElementById("#current-0")
+    val.textContent=dice
     dice.src = "dice-"+ num + ".png";
 });
+
