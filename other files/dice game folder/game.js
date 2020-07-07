4c0b0af4aae243b5a6b874804636856b631fc466
Chandan Kumar <descentchandan@gmail.com>
2020-07-08 00:09:43 +0530

    made new function

--- other files/dice game folder/game.js
+++ other files/dice game folder/game.js
@@ -20,16 +20,33 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
     roundScore += dice;
     document.getElementById("current-" + activePlayer).textContent = roundScore;
   } else {
-    
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-    roundScore = 0;
-
-    document.querySelector(".dice").style.display = "none";
-    document.getElementById("current-0").textContent = "0";
-    document.getElementById("current-1").textContent = "0";
-
-    document.querySelector(".player-0-panel").classList.toggle("active");
-    document.querySelector(".player-1-panel").classList.toggle("active");
-    
+    nextPlayer();
   }
 });
+
+
+
+document.querySelector(".btn-hold").addEventListener("click", function () {
+    document.querySelector 
+
+
+})
+
+
+
+
+
+
+
+
+function nextPlayer() {
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+
+  document.querySelector(".dice").style.display = "none";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+}
