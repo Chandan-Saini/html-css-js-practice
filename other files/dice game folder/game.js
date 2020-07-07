3fd8e4d8c90dbcac13c8e52cc71439eb0e3dcd67
Chandan Kumar <descentchandan@gmail.com>
2020-07-07 21:01:46 +0530

    var added

--- other files/dice game folder/game.js
+++ other files/dice game folder/game.js
@@ -1,5 +1,11 @@
 
-var num= Math.floor(Math.random()*6) +1
+var scores, roundScore, activePlayer, dice;
+scores=[0,0]
+roundScore=0
+activePlayer=1
+
+var dice = Math.floor(Math.random()*6) +1
+
 document.querySelector(".btn-roll").addEventListener("click", function () {
     dice = document.querySelector(".dice")
     val = document.getElementById("current-0")
