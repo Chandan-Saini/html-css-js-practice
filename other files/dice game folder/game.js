6fb420a0b26ee8f94e140ac50b875f694bac003f
Chandan Kumar <descentchandan@gmail.com>
2020-07-07 20:33:21 +0530

    making work roll and current

--- other files/dice game folder/game.js
+++ other files/dice game folder/game.js
@@ -2,8 +2,8 @@
 var num= Math.floor(Math.random()*6) +1
 document.querySelector(".btn-roll").addEventListener("click", function () {
     dice = document.querySelector(".dice")
-    val = document.getElementById("#current-0")
-    val.textContent=dice
+    val = document.getElementById("current-0")
+    val.textContent= num
     dice.src = "dice-"+ num + ".png";
 });
 
