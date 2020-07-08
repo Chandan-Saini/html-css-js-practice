b79e1635f7002ab20af7e6a3597670822cc40eea
Chandan Kumar <descentchandan@gmail.com>
2020-07-08 13:15:22 +0530

    done new button

--- other files/dice game folder/game.js
+++ other files/dice game folder/game.js
@@ -66,8 +66,8 @@ function init() {
   document.getElementById("current-0").textContent = "0";
   document.getElementById("current-1").textContent = "0";
 
-  document.getElementById("name-0").classList.remove("winner")
-  document.getElementById("name-1").classList.remove("winner")
+  document.querySelector(".player-0-panel").classList.remove("winner")
+  document.querySelector(".player-1-panel").classList.remove("winner")
   document.getElementById("name-0").textContent="Player 1"
   document.getElementById("name-1").textContent="Player 2"
   document.querySelector(".player-0-panel").classList.remove("active")
