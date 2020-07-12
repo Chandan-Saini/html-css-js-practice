2eea4903abc2593095f9e5611993f7d932cde7e8
Chandan Kumar <descentchandan@gmail.com>
2020-07-12 21:01:39 +0530

    reading input data

--- other files/budget app folder/budget.js
+++ other files/budget app folder/budget.js
@@ -1,17 +1,49 @@
 var budgetController = (function () {})();
 
-var UIcontroller = (function () {})();
-
-var controller = (function (budgcntrl, UIcntrl) {
-  function keyPress() {
-    console.log("Yes it works");
+var UIcontroller = (function () {
+  
+  var DOMstrings= {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn"
   }
 
-  document.querySelector(".add__btn").addEventListener("click", keyPress);
+  return {
+    getinput:function () {
+      
+      return {
+        type: document.querySelector( DOMstrings.inputType).value,         //it will give val like inc exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      }
+    },
+    getDOMstrings:function () {
+      return DOMstrings
+    }
+      
+  }
+
+})();
+
+var controller = (function (budgcntrl, UIcntrl) {
+  
+  var DOM = UIcntrl.getDOMstrings()
+  
+  var ctrlAddItem= function () {
+
+    var input = UIcntrl.getinput()
+    console.log( input);
+    console.log("Yes it works");
+  }
+  
+  
+
+  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
   document.addEventListener("keypress", function (event) {
     if (event.keyCode === 13 || event.which === 13) {
-      keyPress();
+      ctrlAddItem();
     }
   });
 })(budgetController, UIcontroller);
