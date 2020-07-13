4ff93dadf6c70bd35817eb0583e2b8879b89b796
Chandan Kumar <descentchandan@gmail.com>
2020-07-13 14:52:27 +0530

    done adding new items to budget controller

--- other files/budget app folder/budget.js
+++ other files/budget app folder/budget.js
@@ -1,98 +1,101 @@
 var budgetController = (function () {
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
 
-  var Expence= function (id,description,value) {
-    this.id=id
-    this.description=description
-    this.value=value
-  }
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
 
-  var Income= function (id,description,value) {
-    this.id=id
-    this.description=description
-    this.value=value
-  }
-
-  var data={
-    allItems:{
-      exp:[],
-      inc:[]
+  var data = {
+    allItems: {
+      exp: [],
+      inc: [],
     },
-    totals:{
-      exp:0,
-      inc:0
-    }
-  }
+    totals: {
+      exp: 0,
+      inc: 0,
+    },
+  };
 
   return {
+    addItem: function (type, des, val) {
+      var newItem, ID;
 
-    addItems: function (type,des,val) {
-        
-       var item= data.allItems[type].push()
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0; 
+      }
 
-       return item
-    }
-  }
+      if (type === "exp") {
+        newItem = new Expense(ID, des, val);
+      } else if (type === "inc") {
+        newItem = new Income(ID, des, val);
+      }
 
+      data.allItems[type].push(newItem);
+      return newItem;
+    },
+    testing: function () {
+      console.log(data);
+    },
+  };
 })();
 
-
 var UIcontroller = (function () {
-  
-  var DOMstrings= {
+  var DOMstrings = {
     inputType: ".add__type",
     inputDescription: ".add__description",
     inputValue: ".add__value",
-    inputBtn: ".add__btn"
-  }
+    inputBtn: ".add__btn",
+  };
 
   return {
-    getinput:function () {
-      
+    getinput: function () {
       return {
-        type: document.querySelector( DOMstrings.inputType).value,         //it will give val like inc exp
+        type: document.querySelector(DOMstrings.inputType).value, //it will give val like inc exp
         description: document.querySelector(DOMstrings.inputDescription).value,
-        value: document.querySelector(DOMstrings.inputValue).value
-      }
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
     },
-    getDOMstrings:function () {
-      return DOMstrings
-    }
-      
-  }
-
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+  };
 })();
 
-var controller = (function (budgcntrl, UIcntrl) {
-  
-  
-  
-  var setupEventListeners= function() {
+var controller = (function (budgetCtrl, UICtrl) {
+  var setupEventListeners = function () {
+    var DOM = UICtrl.getDOMstrings();
 
-           var DOM = UIcntrl.getDOMstrings()
-           
-           document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
-           document.addEventListener("keypress", function (event) {
-             if (event.keyCode === 13 || event.which === 13) {
-               ctrlAddItem();
-             }
-           });
-  }
-
-  var ctrlAddItem= function () {
-
-    var input = UIcntrl.getinput()
-    console.log( input);
-  }
-  
-    return {
-      init: function() {
-        console.log("Application has started");
-        setupEventListeners()
+    document.addEventListener("keypress", function (event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
       }
-    }
+    });
+  };
 
+  var ctrlAddItem = function () {
+    var input, newItem;
+
+    input = UICtrl.getinput();
+
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+  };
+
+  return {
+    init: function () {
+      console.log("Application has started");
+      setupEventListeners();
+    },
+  };
 })(budgetController, UIcontroller);
 
-controller.init()
-
+controller.init();
