fb29d123841e600226258fd1c4bee8d66d8a6d3e
Chandan Kumar <descentchandan@gmail.com>
2020-07-14 00:52:27 +0530

    showing the budget values in web page

--- other files/budget app folder/budget.js
+++ other files/budget app folder/budget.js
@@ -87,6 +87,10 @@ var UIcontroller = (function () {
     inputBtn: ".add__btn",
     incomeContainer: ".income__list",
     expensesContainer: ".expenses__list",
+    budgetLabel: ".budget__value",
+    incomeLabel: ".budget__income--value",
+    expensesLabel: ".budget__expenses--value",
+    percentageLabel: ".budget__expenses--percentage",
   };
 
   return {
@@ -130,6 +134,19 @@ var UIcontroller = (function () {
       });
       fieldsArr[0].focus();
     },
+    displayBudget: function (obj) {
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expensesLabel).textContent =
+        obj.totalExp;
+
+      if (obj.percentage > 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent =
+          obj.percentage + "%";
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent = "---";
+      }
+    },
     getDOMstrings: function () {
       return DOMstrings;
     },
@@ -156,7 +173,7 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     var budget = budgetCtrl.getBudget();
 
-    console.log(budget);
+    UICtrl.displayBudget(budget);
   };
 
   var ctrlAddItem = function () {
@@ -178,6 +195,12 @@ var controller = (function (budgetCtrl, UICtrl) {
   return {
     init: function () {
       console.log("Application has started");
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1,
+      });
       setupEventListeners();
     },
   };
