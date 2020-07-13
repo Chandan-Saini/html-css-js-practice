ddb1063b82ac07bfe3f237da3923b238c71fa53a
Chandan Kumar <descentchandan@gmail.com>
2020-07-13 17:03:53 +0530

    clearing input fields

--- other files/budget app folder/budget.js
+++ other files/budget app folder/budget.js
@@ -80,6 +80,19 @@ var UIcontroller = (function () {
 
       document.querySelector(element).insertAdjacentHTML('beforeend',newHtml)
     },
+     
+    clearFields: function () {
+      var fields, fieldsArr;
+
+      fields= document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue)
+
+      fieldsArr= Array.prototype.slice.call(fields)
+
+      fieldsArr.forEach(function (current, index,array) {
+        current.value=""
+      })
+      fieldsArr[0].focus()
+    },
     getDOMstrings: function () {
       return DOMstrings;
     }
@@ -107,6 +120,8 @@ var controller = (function (budgetCtrl, UICtrl) {
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
     UICtrl.addListItem(newItem, input.type)
+
+    UICtrl.clearFields()
   };
 
   return {
