3d97ee5cf0e2860114964fe02811dd07759a207d
Chandan Kumar <descentchandan@gmail.com>
2020-07-15 00:30:06 +0530

    formated the numbers with commas

--- other files/budget app folder/budget.js
+++ other files/budget app folder/budget.js
@@ -131,8 +131,24 @@ var UIcontroller = (function () {
     expensesLabel: ".budget__expenses--value",
     percentageLabel: ".budget__expenses--percentage",
     container: ".container",
-    expensesPercLabel:".item__percentage"
+    expensesPercLabel: ".item__percentage",
+  };
 
+  var formatNumber = function (num, type) {
+    var numSplit, int, dec, type;
+    num = Math.abs(num);
+    num = num.toFixed(2);
+
+    numSplit = num.split(".");
+
+    int = numSplit[0];
+    if (int.length > 3) {
+      int = int.substr(0, int.length - 3) + "," + int.substr(int.length - 3, 3);
+    }
+
+    dec = numSplit[1];
+
+    return (type === "exp" ? "-" : "+") + " " + int + "." + dec;
   };
 
   return {
@@ -152,12 +168,12 @@ var UIcontroller = (function () {
       } else if (type === "exp") {
         element = DOMstrings.expensesContainer;
         html =
-          '<div class="item clearfix" id="exp-%id%""><div div class="item__description" > %description%</div ><div class="right clearfix"><div class="item__value">- %value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div >';
+          '<div class="item clearfix" id="exp-%id%""><div div class="item__description" > %description%</div ><div class="right clearfix"><div class="item__value"> %value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div >';
       }
 
       newHtml = html.replace("%id%", obj.id);
       newHtml = newHtml.replace("%description%", obj.description);
-      newHtml = newHtml.replace("%value%", obj.value);
+      newHtml = newHtml.replace("%value%", formatNumber(obj.value, type));
 
       document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
     },
@@ -182,10 +198,16 @@ var UIcontroller = (function () {
       el.parentNode.removeChild(el);
     },
     displayBudget: function (obj) {
-      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
-      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      var type;
+      obj.budget > 0 ? (type = "inc") : (type = "exp");
+
+      document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(
+        obj.budget,
+        type
+      );
+      document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc, "inc");
       document.querySelector(DOMstrings.expensesLabel).textContent =
-        obj.totalExp;
+        formatNumber(obj.totalExp,"exp");
 
       if (obj.percentage > 0) {
         document.querySelector(DOMstrings.percentageLabel).textContent =
@@ -196,21 +218,21 @@ var UIcontroller = (function () {
     },
 
     displayPercentages: function (percentages) {
-       var fields= document.querySelectorAll(DOMstrings.expensesPercLabel)
-      
-      var nodeListForEach= function (list,callback) {
-        for(var i=0; i<list.length ; i++){
-            callback(list[i],i)
-        }
-      }
+      var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-       nodeListForEach(fields,function (current,index) {
-            if (percentages[index] > 0) {
-               current.textContent= percentages[index] + '%'
-            }else{
-              current.textContent="---"
-            }
-       })
+      var nodeListForEach = function (list, callback) {
+        for (var i = 0; i < list.length; i++) {
+          callback(list[i], i);
+        }
+      };
+
+      nodeListForEach(fields, function (current, index) {
+        if (percentages[index] > 0) {
+          current.textContent = percentages[index] + "%";
+        } else {
+          current.textContent = "---";
+        }
+      });
     },
 
     getDOMstrings: function () {
