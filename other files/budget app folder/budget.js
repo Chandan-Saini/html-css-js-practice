3a1fe426812f957f96dacba58f468e99dda2165d
Chandan Kumar <descentchandan@gmail.com>
2020-07-15 16:18:52 +0530

    delete icon added

--- other files/budget app folder/budget.js
+++ other files/budget app folder/budget.js
@@ -172,11 +172,11 @@ var UIcontroller = (function () {
       if (type === "inc") {
         element = DOMstrings.incomeContainer;
         html =
-          '<div class="item clearfix" id="inc-%id%"><div div class="item__description" > %description%</div ><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div >';
+          '<div class="item clearfix" id="inc-%id%"><div div class="item__description" > %description%</div ><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="far fa-times-circle"></i></button></div></div></div >';
       } else if (type === "exp") {
         element = DOMstrings.expensesContainer;
         html =
-          '<div class="item clearfix" id="exp-%id%""><div div class="item__description" > %description%</div ><div class="right clearfix"><div class="item__value"> %value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div >';
+          '<div class="item clearfix" id="exp-%id%""><div div class="item__description" > %description%</div ><div class="right clearfix"><div class="item__value"> %value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="far fa-times-circle"></i></button></div></div></div >';
       }
 
       newHtml = html.replace("%id%", obj.id); //this is used to replace strings in upper html strings
