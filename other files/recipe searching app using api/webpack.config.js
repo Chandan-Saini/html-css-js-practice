1fe557f74279998a2293dd7fb84d13883696c9cf
Chandan Kumar <descentchandan@gmail.com>
2020-07-20 22:25:01 +0530

    config webpack code and made test.js to check it

---
+++ other files/recipe searching app using api/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/js/index.js", //form here bundling starts
+  output: {                   //here bundling will be stored or saved
+    path: path.resolve(__dirname, "dist/js"), //this path should be absolute so we require path, and here dirname is current absolute path which will get adds in dist/js
+    filename: "bundle.js",
+  },
+  mode:'development'
+};
