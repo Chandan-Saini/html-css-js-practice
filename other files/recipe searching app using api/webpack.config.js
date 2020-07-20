e4d954333e59caadc44ff1f64aa0b2377a18df9b
Chandan Kumar <descentchandan@gmail.com>
2020-07-21 00:42:45 +0530

    installed wepack-dev-server and configures it

--- other files/recipe searching app using api/webpack.config.js
+++ other files/recipe searching app using api/webpack.config.js
@@ -2,8 +2,12 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/js/index.js", //form here bundling starts
-  output: {                   //here bundling will be stored or saved
+  output: {
+    //here bundling will be stored or saved
     path: path.resolve(__dirname, "dist/js"), //this path should be absolute so we require path, and here dirname is current absolute path which will get adds in dist/js
     filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: "./dist", //this is the path where webpack have to store data
   },
 };
