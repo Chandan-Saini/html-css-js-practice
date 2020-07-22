7e55970e31e86d638d64d8b40e465a12c386c090
Chandan Kumar <descentchandan@gmail.com>
2020-07-22 23:00:40 +0530

    this code is giving errors, trying to solve

--- other files/recipe searching app using api/webpack.config.js
+++ other files/recipe searching app using api/webpack.config.js
@@ -1,27 +1,27 @@
-const path = require("path");
-const HtmlWebpackPlugin= require("html-webpack-plugin")
+const path = require('path');
+const HtmlWebpackPlugin= require('html-webpack-plugin')
 
 module.exports = {
-  entry: ["babel-polyfill","./src/js/index.js"], //form here bundling starts
+  entry: ['babel-polyfill','./src/js/index.js'], //fromm here bundling starts
   output: {
     //here bundling will be stored or saved
-    path: path.resolve(__dirname, "dist"), //this path should be absolute so we require path, and here dirname is current absolute path which will get adds in dist/js,
+    path: path.resolve(__dirname, 'dist'), //this path should be absolute so we require path, and here dirname is current absolute path which will get adds in dist/js,
                                               // now earlier we have written path as dist/js but it was not helping to run webpack dev server automatically so we changed the path to dist only
-    filename: "js/bundle.js"             //earlier here path was only name of file bundle.js but webpack server was not running automatically so we used path as js/bundle.js
+    filename: 'js/bundle.js'             //earlier here path was only name of file bundle.js but webpack server was not running automatically so we used path as js/bundle.js
   },
   devServer: {
-    contentBase: "./dist" //this is the path where webpack have to store data
+    contentBase: './dist' //this is the path where webpack have to store data
   },
   plugins:[
     new HtmlWebpackPlugin({
-      filename:"index.html",
-      template:"./src/index.html"
+      filename:'index.html',
+      template:'./src/index.html'
     })
   ],
   module:{                   //it contains code for loader
     rules:[
       {
-        test: /\.js$/,      //this line means loader need to find files ends with .js and work on that
+        test:/\.js$/,      //this line means loader need to find files ends with .js and work on that
         exclude:/node_modules/,    //this line means loader needs to exclude this file to work with
         use:{
           loader:'babel-loader'   //the loader we want to use
