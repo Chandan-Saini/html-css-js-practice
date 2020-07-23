57ba19699c868939170ec062295ce6ad43f95486
Chandan Kumar <descentchandan@gmail.com>
2020-07-23 17:56:49 +0530

    way 2 to import as diff name

--- other files/recipe searching app using api/src/js/index.js
+++ other files/recipe searching app using api/src/js/index.js
@@ -1,6 +1,6 @@
 
 import search from "./models/Search";
-import { sum, mul, id } from "./views/searchView"
+import { sum as s, mul as m, id as i } from "./views/searchView"
 
 
-console.log(`this is a sum ${sum(id, 2)} this is mul ${mul(3,2)} and this is string ${search}`);
+console.log(`this is a sum ${s(i, 2)} this is mul ${m(3,2)} and this is string ${search}`);
