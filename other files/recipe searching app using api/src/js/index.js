6871af7423c096b54629b90aca637a283f89ef6d
Chandan Kumar <descentchandan@gmail.com>
2020-07-25 22:50:41 +0530

    now recipes are showed on site on search

--- other files/recipe searching app using api/src/js/index.js
+++ other files/recipe searching app using api/src/js/index.js
@@ -1,24 +1,27 @@
 import Search from './models/Search'
+import * as searchView from "./views/searchView"
+import { elements } from "./views/base"
 
 const state={}
 
 const controlSearch = async ()=>{
 
-    const query="pizza"
+    const query= searchView.getInput()
     
       state.search = new Search(query);
 
 
      await state.search.getResults();
-     console.log(state.search.result);
+    searchView.renderResults(state.search.result)
+     
     
     
 }
 
-document.querySelector(".search").addEventListener("click", (e) => {
+elements.searchForm.addEventListener("submit", (e) => {
     e.preventDefault()
     controlSearch()
-});
+}); 
 
 
 
