4b3ac53edeb0c2d8b62ae9d48972f483e67702b9
Chandan Kumar <descentchandan@gmail.com>
2020-07-26 17:37:20 +0530

    made Recipe.js in models and config.js in Js

--- other files/recipe searching app using api/src/js/models/Search.js
+++ other files/recipe searching app using api/src/js/models/Search.js
@@ -1,13 +1,12 @@
 import axios from "axios";
-
+import {key,proxy} from '../config'
 export default class Search {
   constructor(query) {
     this.query = query;
   }
 
   async getResults() {
-    const proxy = "https://cors-anywhere.herokuapp.com/";
-    const key = "";
+   
     try {
       const res = await axios(
         `${proxy}https://recipesapi.herokuapp.com/api/search?key=${key}&q=${this.query}`
